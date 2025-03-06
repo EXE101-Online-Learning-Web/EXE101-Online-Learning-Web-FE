e2ef2272fbaf1e384eca363af4c843da76f0e52f
Levando10 <dolevan055@gmail.com>
2025-03-07 03:17:46 +0700

    DoLV2 certificate done

--- src/components/Feature/Certificate/Certificate.js
+++ src/components/Feature/Certificate/Certificate.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
 import { useLocation } from "react-router-dom";
@@ -10,6 +10,10 @@ const Certificate = ({
   instructorName = "Jane Smith",
   completionDate = new Date().toLocaleDateString(),
 }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
+
   const certificateRef = useRef();
   const location = useLocation();
   const certificateData = location.state;
